ec84bc10294d596527b9a7df536d4a4adc6a9953
Olga0103 <nezabudka-84@mail.ru>
2021-04-29 20:20:35 +0300

    Initial commit

---
+++ src/main/java/ru/gb/april/market/april_market/AprilMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.april.market.april_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprilMarketApplication.class, args);
+	}
+
+}
